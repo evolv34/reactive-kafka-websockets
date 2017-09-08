57c9bed7265c48490e315a770d9ddfe7dbcb741f
evolv34 <c20566715@gmail.com>
2017-09-08 08:54:28 -0500

    update websocket client

--- ws-client/client.js
+++ ws-client/client.js
@@ -1,6 +1,13 @@
 var WebSocket = require('ws');
-var ws = new WebSocket('ws://localhost:9093/kafka',{
-        headers : {
-          token: "AAAA%2FAAA%3DAAAAAAAA"
-        }
-      });
+var ws = new WebSocket('ws://localhost:9093/produce',{
+    headers : {
+        token: "AAAA%2FAAA%3DAAAAAAAA"
+    }
+});
+
+ws.on('open', function open() {
+    console.log("sending message ...");
+    for(i = 0; i < 10; i ++) {
+        ws.send('{"topic":"hello","content":"world - ' + i + '"}');
+    }
+});
